a838b8ce9883dfef72c9bba8b336dfc63e5b0f60
guanguan <guanguan@cc.cn>
2019-06-14 21:09:02 +0800

    delete-ssm

--- mavendemotwo/maven_two_dao/src/main/java/com/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-import com.domain.User;
-
-public interface UserDao {
-    User findUserById(int i);
-}
--- mavendemotwo/maventwoservice/src/main/java/com/service/UserServiceInter.java
+++
@@ -1,7 +0,0 @@
-package com.service;
-
-import com.domain.User;
-
-public interface UserServiceInter {
-    User findUserById(int i);
-}
